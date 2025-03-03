3a15d8102456fc428a2ae6519d740aef5b26416d
Macari Stanislav <stasmacari63@gmail.com>
2025-03-03 21:34:46 +0200

    Updates

--- src/components/mainPages/profile/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "../../css/Profile.css";
-
-const Profile = () => {
-  return <div></div>;
-};
-
-export default Profile;
--- src/routs/routes.jsx
+++
@@ -1,7 +0,0 @@
-const routes = {
-  standart: "/",
-  main: "/main",
-  register: "/register",
-};
-
-export default routes;
